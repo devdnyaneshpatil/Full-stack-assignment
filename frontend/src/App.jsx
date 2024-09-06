743e0753a718573cff3132db324fb450b8229b5b
devdnyaneshpatil <dnyaneshwarpatil830831@gmail.com>
2024-09-06 17:24:47 +0530

    feature- loginUi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './Pages/Login'
+import HomePage from './Pages/HomePage'
+import Signup from './Pages/Signup'
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path='/' Component={Login} />
+        <Route path='/home' Component={HomePage} />
+        <Route path='/sign-up' Component={Signup}/>
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,164 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+
+function Login() {
+  //state and handling of show password
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleBlur = (e) => {
+    const { name } = e.target;
+    setTouched({ ...touched, [name]: true });
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  const isEmailError = formData.email === "" || !validateEmail(formData.email);
+  const isPasswordError =
+    formData.password === "" || formData.password.length < 8;
+
+  return (
+    <>
+      <Box
+        w="100%"
+        h="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        bg="gray.50"
+      >
+        <Container
+          maxW="sm"
+          p="6"
+          borderRadius="lg"
+          boxShadow="lg"
+          h={{ base: "100vh", md: "auto" }}
+          bg={{ base: "gray.100", md: "white" }}
+        >
+          <Box display="flex" justifyContent="center" mb="6">
+            <Avatar
+              size="xl"
+              name="User Avatar"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEltzX7n3ZelAoJe4mlsBpziUHSG1ko6URJGibMQKr6pJEmN3M_5iixXoLIy_uCgjgPSA&usqp=CAU"
+            />
+          </Box>
+          <Heading textAlign="center" mb="6" fontSize="2xl">
+            Login
+          </Heading>
+
+          <form onSubmit={handleSubmit}>
+            <FormControl isInvalid={touched.email && isEmailError}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                name="email"
+                onBlur={handleBlur}
+                borderColor={
+                  touched.email && isEmailError ? "red.500" : "gray.300"
+                }
+              />
+              {!touched.email || !isEmailError ? (
+                <FormHelperText>
+                  Enter the email you'd like to use for login.
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  Please enter a valid email*.
+                </FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={touched.password && isPasswordError} mt="4">
+              <FormLabel>Password</FormLabel>
+              <InputGroup size="md">
+                <Input
+                  type={show ? "text" : "password"}
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  name="password"
+                  onBlur={handleBlur}
+                  borderColor={
+                    touched.password && isPasswordError ? "red.500" : "gray.300"
+                  }
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show ? <FaEyeSlash /> : <FaEye />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              {!touched.password || !isPasswordError ? (
+                <FormHelperText></FormHelperText>
+              ) : (
+                <FormErrorMessage>
+                  Password must be at least 8 characters*.
+                </FormErrorMessage>
+              )}
+            </FormControl>
+            <Button
+              colorScheme="blue"
+              mt="6"
+              w="full"
+              isDisabled={isEmailError || isPasswordError}
+              type="submit"
+            >
+              Login
+            </Button>
+          </form>
+          <Text textAlign={"center"} mt={"3"}>
+            Don't have an account?
+            <Link to="/sign-up" style={{ color: "blue", fontWeight: "bold" }}>
+              Signup
+            </Link>
+          </Text>
+          <Button w="full" mt={4} colorScheme="red" variant="outline" gap={"2"}>
+            Continue with
+            <FcGoogle size={"30"} />
+          </Button>
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+export default Login;
